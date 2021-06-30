5586338cee49afda63a69c1390d35ca37a23f758
shubh817 <59890154+shubh817@users.noreply.github.com>
2021-06-30 22:32:41 +0530

    Create Reverse me.java

---
+++ Reverse me.java
@@ -0,0 +1,31 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		try{
+            Scanner sc = new Scanner(System.in);
+        	int n=sc.nextInt();
+        	int a[]=new int[n];
+        	for(int i=0;i<n;i++)
+        	{
+        	    a[i]=sc.nextInt();
+        	}
+        	for(int i=a.length-1;i>=0;i--)
+        	{
+        	    System.out.print(a[i]+" ");
+        	}		    
+		}
+		catch(Exception e)
+		{
+		    System.out.println(e);
+		}
+	}
+}
